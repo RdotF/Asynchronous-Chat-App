f17f73c0850f7313a11eb9d12b75ef4184bdafc4
RdotF <tsarev02101977@gmail.com>
2025-08-24 02:20:21 +0300

    distinguished between users, css update

--- handlers.py
+++ handlers.py
@@ -1,6 +1,6 @@
 import aiohttp_jinja2
 from aiohttp import web
-
+import json
 active_ws = set()
 
 @aiohttp_jinja2.template('index.html')
@@ -10,17 +10,20 @@ async def index(request):
 async def websocket_handler(request):
   ws = web.WebSocketResponse()
   await ws.prepare(request)
-  active_ws.add(ws)
+  current_user_id = request.query.get('user_id', 'guest')
+  active_ws.add(ws) 
   try:
     async for msg in ws:
       if msg.type == web.WSMsgType.TEXT:
-        print(f'Received message: {msg.data}')
-        print('active_ws', active_ws)
+        print(f'Received message: {msg.data}\nType - {type(msg.data)}')
         for peer in active_ws:
-          
           print('Sending the message...')
-          await peer.send_str(msg.data)
-          print(f'Sent message: {msg.data}')
+          message_data = {
+                            'userId': current_user_id,  
+                            'text': msg.data
+                        }
+          await peer.send_str(json.dumps(message_data))
+          print(f'Sent message: {current_user_id} - {msg.data}')
   finally:
     active_ws.remove(ws)
   return ws
--- static/client.js
+++ static/client.js
@@ -1,20 +1,29 @@
+function generateUserId() {
+    return 'user_' + Math.floor(Math.random() * 10000);  // e.g. user_1234
+}
+const currentUserId = generateUserId()
+const ws = new WebSocket(`ws://${location.host}/ws?user_id=${currentUserId}`)
 
-const ws = new WebSocket(`ws://${location.host}/ws`)
 ws.onopen = () => {
     console.log('Connected to the WebSocket server.');
+    console.log(currentUserId)
 };
-ws.onmessage = (event) => {
-  console.log('onmessage', event.data)
-  const messages = document.getElementById('messages');
-  const msg = document.createElement('li');
 
-  msg.textContent = event.data;
-  messages.appendChild(msg);
+ws.onmessage = (event) => {
+    console.log('onmessage', event.data);
+    const messages = document.getElementById('messages');
+    const msg = document.createElement('li');
+
+    const messageData = JSON.parse(event.data);
+    msg.className = messageData.userId === currentUserId ? 'my-message' : 'peer-message'
+
+    msg.textContent = `${messageData.userId}: ${messageData.text}`;  
+    messages.appendChild(msg)
 }
 
 document.getElementById('send').onclick = () => {
  const input = document.getElementById('msg');
   console.log('Sending message:', input.value);  
-  ws.send(input.value);
+  ws.send( input.value )
   input.value = '';
 }
